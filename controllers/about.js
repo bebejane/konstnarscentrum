36183ae0340d7ec5721c5a0cf1469d54b4c963a2
Bebe Jane <bjornthief@gmail.com>
2021-10-29 16:59:24 +0200

    multi abouts

--- controllers/about.js
+++ controllers/about.js
@@ -1,9 +1,13 @@
 import { apiQuery } from "/lib/dato";
-import { GetAbout } from "/graphql/About.graphql";
+import { GetAbout, GetAllAbouts } from "/graphql/About.graphql";
 
 export default {
 	get: async (preview) => {
 		const { about } = await apiQuery(GetAbout, {}, preview);
 		return about
 	},
+	all: async (preview) => {
+		const { abouts } = await apiQuery(GetAllAbouts, {}, preview);
+		return abouts
+	}
 };
--- pages/about/index.js
+++ pages/about/index.js
@@ -3,32 +3,41 @@ import { aboutController } from "/controllers";
 import { StructuredText, Image } from "react-datocms";
 
 
-export default function About({ about }) {
-	console.log(about)
+export default function About({ about, abouts }) {
+	console.log(abouts)
 	return (
 		<div className={styles.container}>
-			<h1>About</h1>
-			<StructuredText 
-				data={about.content} 
-				renderBlock={({ record }) => {
-					console.log(record)
-          switch (record.__typename) {
-            case 'ImageRecord':
-              return <Image data={record.image.responsiveImage} />;
-            default:
-              return null;
-          }
-        }}
-			/>
+			{abouts.map((about)=>
+				<>
+				<h1>{about.title}</h1>
+				<StructuredText 
+					data={about.content} 
+					renderBlock={({ record }) => {
+						console.log(record)
+						switch (record.__typename) {
+							case 'ImageRecord':
+								return <Image data={record.image.responsiveImage} />;
+							case 'HeadlineRecord':
+								return <h2>{record.headline}</h2>;
+							default:
+								return null;
+						}
+					}}
+				/>
+				<hr/>
+				</>
+			)}
 		</div>
 	);
 }
 
 export async function getServerSideProps(context) {
 	const about = await aboutController.get();
+	const abouts = await aboutController.all();
 	return {
 		props: {
-			about
+			about,
+			abouts
 		},
 	};
 }
