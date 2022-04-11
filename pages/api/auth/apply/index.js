b61ba3fe7b6dc8fa1acbb6637ba5ffcdd870f832
Bebe Jane <bjornthief@gmail.com>
2022-04-11 09:42:26 +0200

    test

--- pages/api/auth/apply/index.js
+++ pages/api/auth/apply/index.js
@@ -18,9 +18,12 @@ export default catchErrorsFrom( async (req, res) => {
   const roles = await Dato.accessTokens.all();
   const roleAPIToken = roles.filter((t) => t.role === roleId)[0].token
   const approvalToken =  await generateToken(email)
-  const RoleClient = new SiteClient(roleAPIToken);
+
   console.log(roleAPIToken, roleId)
   console.log(roles)
+  
+  const RoleClient = new SiteClient(roleAPIToken);
+  
   const application = await RoleClient.items.create({
     itemType:applicationModelId,
     email,
