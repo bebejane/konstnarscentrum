430b22185488ea7d277738fdcc2c1c51cddc8fb7
Bebe Jane <bjornthief@gmail.com>
2022-11-05 08:09:34 +0100

    test templates

---
+++ pages/api/email/index.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function(req: NextApiRequest, res: NextApiResponse){
+
+  const{ template, params } = req.query
+
+
+
+
+}
