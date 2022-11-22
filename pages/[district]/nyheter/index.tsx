3667eddb0e2baf20c54bc9c4e5b4e79917c712fa
Bebe Jane <bjornthief@gmail.com>
2022-11-22 12:19:18 +0100

    updates

--- pages/[district]/nyheter/index.tsx
+++ pages/[district]/nyheter/index.tsx
@@ -5,9 +5,11 @@ import { districts, apiTokenByDistrict } from "/lib/district";
 import { apiQuery } from "dato-nextjs-utils/api";
 import { AllNewsDocument } from "/graphql";
 import { format } from "date-fns";
+import client from "/lib/client";
 
-export default function News({ news, district }) {
-
+export default function News({ news, news2, district }) {
+	console.log(news2);
+	
 	return (
 		<div className={styles.container}>
 			<h1>{district.name} News</h1>
@@ -28,14 +30,22 @@ export { getStaticPaths } from '../index'
 
 export const getStaticProps : GetStaticProps = withGlobalProps({queries:[]}, async ({props, revalidate, context } : any) => {
 	
+
 	const district = districts.find(d => d.slug === context.params.district)
 	console.log(apiTokenByDistrict(district.slug));
-	
+	console.log(district)
+	const news2 = (await client.items.list({
+		filter:{
+			type:"news"
+		}
+	})).filter(n => n.creator.id === district.tokenId)
+
 	const { news } = await apiQuery(AllNewsDocument, {apiToken: apiTokenByDistrict(district.slug)})
 	
 	return {
 		props: {
 			news,
+			news2,
 			district
 		},
 	};
