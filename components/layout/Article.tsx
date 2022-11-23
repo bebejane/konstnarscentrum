f1aea9e5aaa8800c8dce2a91aebd054cb44bb65a
Bebe Jane <bjornthief@gmail.com>
2022-11-23 14:32:32 +0100

    fix

--- components/layout/Article.tsx
+++ components/layout/Article.tsx
@@ -18,11 +18,11 @@ export default function Article({ children, title, text, image, content }: Artic
     <div className={s.article}>
       {image ?
         <header>
-          <h1>{title}</h1>
+          <h1 className={s.title}>{title}</h1>
           <Image className={s.image} data={image.responsiveImage} />
         </header>
         :
-        <h1> {title}</h1>
+        <h1 className={s.title}> {title}</h1>
       }
       <Markdown className="intro">
         {text}
