076ca055a6da7e5d7bc06404f61c386df5df0b59
Bebe Jane <bjornthief@gmail.com>
2022-11-27 19:04:38 +0100

    gallery

--- components/common/Gallery.tsx
+++ components/common/Gallery.tsx
@@ -1,25 +1,56 @@
 import s from './Gallery.module.scss'
+import cn from 'classnames'
 import { Image } from 'react-datocms'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export type Props = {
   slides: SlideRecord[]
 }
 
+const parseRecord = (record: any) => {
+  if (!record)
+    return { type: '', slug: '/' }
+  const { __typename, slug } = record
+
+  switch (__typename) {
+    case 'CommissionRecord':
+      return { type: 'Uppdrag', slug: `/anlita-oss/uppdrag/${slug}` }
+    case 'MemberNewsRecord':
+      return { type: 'Aktuellt', slug: `/konstnar/aktuellt/${slug}` }
+    case 'NewsRecord':
+      return { type: 'Nyheter', slug: `/nyheter/${slug}` }
+    case 'ForArtistRecord':
+      return { type: 'För konstnärer', slug: `/` }
+    default:
+      return { type: '', slug: '/' }
+  }
+}
+
 export default function Gallery({ slides }: Props) {
+
+  const [index, setIndex] = useState(0)
+  console.log(slides.map(el => ({ ...el, ...parseRecord(el.link) })));
+
   return (
     <section className={s.gallery}>
       <ul>
-        {slides.map(({ id, headline, image, link }, idx) =>
-          <li key={idx}>
-            <Link href={'/'}>
-              <>
-                <header>
-                  <h5>{'Type'}</h5>
-                  <h2>{headline}</h2>
-                </header>
-                <Image className={s.image} data={image.responsiveImage} objectFit="cover" />
-              </>
+        {slides.map(el => ({ ...el, ...parseRecord(el.link) })).map(({ id, headline, image, link, slug, type }, idx) =>
+          <li
+            key={idx}
+            className={cn(idx === index ? s.transition : s.hide)}
+            onAnimationEnd={() => setIndex(index + 1 > slides.length - 1 ? 0 : index + 1)}
+          >
+            <Link href={slug}>
+              <header>
+                <h5>{type}</h5>
+                <h2>{headline}</h2>
+              </header>
+              <Image
+                className={s.image}
+                data={image.responsiveImage}
+                objectFit="cover"
+              />
             </Link>
           </li>
         )}
--- components/nav/MenuDesktop.tsx
+++ components/nav/MenuDesktop.tsx
@@ -13,12 +13,13 @@ export type MenuDesktopProps = { items: Menu }
 
 export default function MenuDesktop({ items }: MenuDesktopProps) {
 
-	const ref = useRef();
+	const menuRef = useRef<HTMLDivElement | null>(null);
+	const subRef = useRef<HTMLDivElement | null>(null);
 	const router = useRouter()
 	const [paddingLeft, setPaddingLeft] = useState<string>('0px')
 	const [selected, setSelected] = useState<MenuItem | undefined>()
 	const [showMenu, setShowMenu] = useStore((state) => [state.showMenu, state.setShowMenu])
-	const { isPageBottom, isPageTop, isScrolledUp, scrolledPosition } = useScrollInfo()
+	const { isPageBottom, isPageTop, isScrolledUp, scrolledPosition, viewportHeight } = useScrollInfo()
 
 	useEffect(() => { // Toggle menu bar on scroll
 		//setShowMenu((isScrolledUp && !isPageBottom) || isPageTop)
@@ -29,9 +30,26 @@ export default function MenuDesktop({ items }: MenuDesktopProps) {
 		if (typeof selected === 'undefined')
 			return
 
+
+		const isAtBottom = menuRef.current.getBoundingClientRect().bottom >= viewportHeight //subRef.current.getBoundingClientRect().top
 		const el = document.querySelector<HTMLUListElement>(`[data-menu-type="${selected.type}"]`)
 		const bounds = el.getBoundingClientRect()
+
+
 		setPaddingLeft(`${bounds.left}px`)
+
+		if (isAtBottom) {
+			console.log(getComputedStyle(subRef.current).paddingTop);
+
+			const height = 140//subRef.current.getBoundingClientRect().height - 70
+			window.scrollTo({
+				top: height,
+				behavior: 'smooth'
+			})
+		}
+
+
+
 	}, [selected])
 
 	useEffect(() => {
@@ -40,7 +58,7 @@ export default function MenuDesktop({ items }: MenuDesktopProps) {
 
 	return (
 		<>
-			<nav id="menu" ref={ref} className={cn(s.menu, showMenu && s.show)}>
+			<nav id="menu" ref={menuRef} className={cn(s.menu, showMenu && s.show)}>
 				<ul
 					className={s.nav}
 					onMouseLeave={() => setSelected(undefined)}
@@ -67,7 +85,7 @@ export default function MenuDesktop({ items }: MenuDesktopProps) {
 						<RegionSelector />
 					</li>
 				</ul>
-				<div className={s.background} style={{ paddingLeft }}>
+				<div className={s.background} ref={subRef} style={{ paddingLeft }}>
 					{items.map((item, i) => {
 						return (
 							<ul
@@ -75,7 +93,7 @@ export default function MenuDesktop({ items }: MenuDesktopProps) {
 								data-sub-type={item.type}
 								className={cn(s.sub, selected?.type === item.type && !selected.index && s.show)}
 								onMouseLeave={() => setSelected(undefined)}
-								onMouseMove={() => setSelected(item)}
+								onMouseEnter={() => setSelected(item)}
 							>
 								{item.sub?.map(({ slug, label, regional }, idx) =>
 									<li key={idx}>
