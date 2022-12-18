c0422cb41d8ab40bf59266ad573eafecf8d5d747
Bebe Jane <bjornthief@gmail.com>
2022-12-18 19:38:33 +0100

    feeet

--- components/common/HomeGallery.tsx
+++ components/common/HomeGallery.tsx
@@ -2,7 +2,7 @@ import s from './HomeGallery.module.scss'
 import cn from 'classnames'
 import { Image } from 'react-datocms'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { RevealText } from '/components'
 import { SvgBlob } from 'react-svg-blob';
 import blobshape from "blobshape";
@@ -37,25 +37,42 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const slideTime = 6000
+
 export default function HomeGallery({ slides }: Props) {
 
   const [index, setIndex] = useState(0)
+  const [tIndex, setTIndex] = useState()
   const [loaded, setLoaded] = useState({})
+  const [size, setSize] = useState({ width: 0, height: 0 })
+  const ref = useRef<HTMLUListElement | null>(null)
 
   useEffect(() => {
 
     const interval = setInterval(() => {
       setIndex(index + 1 > slides.length - 1 ? 0 : index + 1)
-    }, 5000)
+    }, slideTime)
 
     return () => clearInterval(interval)
   }, [index, slides])
 
-  //const currentImage = slides[index].image
+  useEffect(() => {
+    if (ref.current === null) return
+    setSize({
+      width: ref.current.clientWidth,
+      height: ref.current.clientHeight
+    })
+  }, [ref])
+
+
+  const currentImage = slides[index]?.image
+  const nextImage = slides[index + 1 > slides.length - 1 ? 0 : index + 1]?.image
+
 
   return (
     <section className={s.gallery}>
-      <ul>
+      <ul ref={ref}>
+
         {slides.map(el => ({ ...el, ...parseRecord(el.link) })).map(({ id, headline, image, link, slug, type, blackText }, idx) =>
           <li
             key={idx}
@@ -72,14 +89,87 @@ export default function HomeGallery({ slides }: Props) {
                 onLoad={() => setLoaded({ ...loaded, [id]: true })}
                 objectFit="cover"
               />
+              {index === idx &&
+                <Transition
+                  image={slides[idx + 1 > slides.length - 1 ? 0 : idx + 1]?.image}
+                  size={size}
+                  start={true}
+                />
+              }
             </Link>
           </li>
         )}
-        <div
-          className={s.color}
-        //style={{ backgroundColor: currentImage.responsiveImage.bgColor }}
-        ></div>
+
       </ul>
-    </section>
+
+    </section >
+  )
+}
+
+
+const Transition = ({ image, size, start }) => {
+
+  const totalBlob = 300
+  const [count, setCount] = useState(0)
+  const [paths, setPaths] = useState([])
+
+  useEffect(() => {
+    const paths = new Array(totalBlob).fill(0).map((e, idx) => {
+      const { path } = blobshape({
+        size: randomInt(100, 400),
+        growth: randomInt(2, 20),
+        edges: randomInt(2, 20),
+        seed: null
+      })
+      return (
+        <path
+          d={path}
+          key={idx}
+          transform={`translate(${randomInt(-200, size.width)},${randomInt(-200, size.height)})`}
+        />
+      )
+    })
+    setPaths(paths)
+  }, [setPaths, size])
+
+  useEffect(() => {
+    if (!start)
+      return setCount(0)
+
+    let interval;
+
+    const timeout = setTimeout(() => {
+      let c = 0;
+      interval = setInterval(() => {
+        if (c > totalBlob)
+          return clearInterval(interval)
+        setCount(++c)
+      }, 10)
+    }, slideTime - 2000)
+    return () => {
+      clearInterval(interval)
+      clearTimeout(timeout)
+    }
+  }, [start, image])
+
+
+  return (
+    <div className={s.color}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${size.width} ${size.height}`}>
+        <defs>
+          <clipPath id="mask">
+            {paths.slice(0, count)}
+          </clipPath>
+        </defs>
+      </svg>
+      <Image
+        className={s.maskImage}
+        data={image.responsiveImage}
+        lazyLoad={false}
+        usePlaceholder={false}
+        objectFit="cover"
+        pictureStyle={{ clipPath: `url(#mask)` }}
+      />
+    </div>
   )
 }
