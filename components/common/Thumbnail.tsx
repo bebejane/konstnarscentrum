5a1b212417ae4e6be095d7c6b2c364786b922073
konst-teknik <information-konst-teknik.se>
2022-12-19 15:54:28 +0100

    Fix

--- components/common/Thumbnail.tsx
+++ components/common/Thumbnail.tsx
@@ -63,6 +63,7 @@ export default function Thumbnail({ image, slug, title, subtitle, regional = tru
       }
       <div className={cn('mid', s.title, s.vertical)}>
         {vertical.map(c => c)}
+        <div className={s.fade}></div>
       </div>
       <span className={cn('mid', s.title, s.horizontal)}>
         <span>{horizontal.map(c => c)}</span>
