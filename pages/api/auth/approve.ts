832c251eda919835197a4fbb16dcdbcbb8678d05
Bebe Jane <bjornthief@gmail.com>
2022-12-20 16:35:31 +0100

    fix

--- pages/api/auth/approve.ts
+++ pages/api/auth/approve.ts
@@ -5,31 +5,33 @@ import Email from '/lib/emails';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-	try {
+	await NextCors(req, res, {
+		methods: ['POST'],
+		origin: '*',
+		optionsSuccessStatus: 200,
+	});
 
-		await NextCors(req, res, {
-			methods: ['POST'],
-			origin: '*',
-			optionsSuccessStatus: 200,
-		});
+	withBasicAuth(async (req, res) => {
+		try {
 
-		const { email, approval_token, first_name, last_name, approved } = req.body;
+			const { email, approval_token, first_name, last_name, approved } = req.body;
 
-		console.log('approve application', email);
+			console.log('approve application', email);
 
-		if (!email || !approval_token || !first_name || !last_name)
-			throw 'Ogitltig data'
+			if (!email || !approval_token || !first_name || !last_name)
+				throw 'Ogitltig data'
 
-		if (approved) {
-			await Email.applicationApproved({ email, token: approval_token, name: `${first_name} ${last_name}` });
-		} else {
-			console.log('already approved')
+			if (approved) {
+				await Email.applicationApproved({ email, token: approval_token, name: `${first_name} ${last_name}` });
+			} else {
+				console.log('already approved')
+			}
+
+			res.status(200).json({ approved });
+
+		} catch (err) {
+			console.error(err);
+			res.status(501).json({ error: err?.message || err });
 		}
-
-		res.status(200).json({ approved });
-
-	} catch (err) {
-		console.error(err);
-		res.status(501).json({ error: err?.message || err });
-	}
+	})
 }
