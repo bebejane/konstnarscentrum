ad595ae3e345fdde9a4794a79701dd34bdf74ab9
Bebe Jane <bjornthief@gmail.com>
2022-12-20 17:23:31 +0100

    test

--- pages/api/auth/approve.ts
+++ pages/api/auth/approve.ts
@@ -5,33 +5,39 @@ import Email from '/lib/emails';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-	await NextCors(req, res, {
-		methods: ['POST'],
-		origin: '*',
-		optionsSuccessStatus: 200,
-	});
+	await NextCors(req, res, { methods: ['POST'], origin: '*', optionsSuccessStatus: 200 });
+	console.log('pass cors')
+	//ithBasicAuth(async (req, res) => {
+	console.log('basic auth passed')
+	try {
+		const basicAuth = req.headers.authorization
+		if (!basicAuth)
+			return res.status(401).send('Access denied')
 
-	withBasicAuth(async (req, res) => {
-		try {
+		const auth = basicAuth.split(' ')[1]
+		const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+		const isAuthorized = user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD
 
-			const { email, approval_token, first_name, last_name, approved } = req.body;
+		if (!isAuthorized)
+			return res.status(401).send('Access denied')
 
-			console.log('approve application', email);
+		const { email, approval_token, first_name, last_name, approved } = req.body;
 
-			if (!email || !approval_token || !first_name || !last_name)
-				throw 'Ogitltig data'
+		console.log('approve application', email);
 
-			if (approved) {
-				await Email.applicationApproved({ email, token: approval_token, name: `${first_name} ${last_name}` });
-			} else {
-				console.log('already approved')
-			}
+		if (!email || !approval_token || !first_name || !last_name)
+			throw 'Ogitltig data'
 
-			res.status(200).json({ approved });
+		if (approved)
+			await Email.applicationApproved({ email, token: approval_token, name: `${first_name} ${last_name}` });
+		else
+			console.log('already approved')
 
-		} catch (err) {
-			console.error(err);
-			res.status(501).json({ error: err?.message || err });
-		}
-	})
+		res.status(200).json({ approved });
+
+	} catch (err) {
+		console.error(err);
+		res.status(501).json({ error: err?.message || err });
+	}
+	//})
 }
