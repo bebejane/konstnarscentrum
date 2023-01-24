b60c4dc2091b25bfcd87e226753ebcff785c169a
Bebe Jane <bjornthief@gmail.com>
2023-01-24 17:26:54 +0100

    cleannn

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
+const webpack = require("webpack");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
 	enabled: process.env.ANALYZE === "true",
 });
-const webpack = require("webpack");
 
 const sassOptions = {
 	includePaths: ["./components", "./pages"],
