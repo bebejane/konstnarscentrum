e54240141c2c8c596fe325417ef4787b888db601
Bebe Jane <bjornthief@gmail.com>
2023-02-02 20:11:59 +0100

    test

--- pages/api/auth/approve.ts
+++ pages/api/auth/approve.ts
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	try {
 
 		const { email, approval_token, first_name, last_name, approved, ping } = req.body;
-
+		console.log(req.body)
 		if (ping) {
 			console.log('ping')
 			return res.status(200).json({ pong: true });
